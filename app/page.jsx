0d1f95647fb54709c0cda3c8ee6e7c9c5ecdd385
Obebe <shkim000905@naver.com>
2026-04-30 14:39:17 +0900

    todo 각 아이템 호출

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,8 @@ export default function Home() {
     <section className="mx-auto max-w-3xl px-6 py-16">
       <div className="flex flex-col gap-6">
         <h1 className="text-center text-3xl font-bold">Todo List Main</h1>
+        <p className="text-center font-bold">서비스 소개</p>
+        <p className="text-center">Todo List 관리 앱</p>
       </div>
       <Link href="/todos">Todo 목록보기</Link>
     </section>
--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -1,7 +1,27 @@
-export default function TodosPage() {
+export default async function TodosPage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/todos`);
+  const todos = await res.json();
+
   return (
     <div>
       <h1>Todo 목록</h1>
+
+      <ul className="grid grid-cols-2 gap-4">
+        {todos.map((todo) => (
+          <li key={todo.id} className="rounded-md border border-black-100 p-4">
+            <h2 className="font-semibold">{todo.title}</h2>
+            <div className="mt-2 flex gap-2 text-xs text-gray-500">
+              <span>카테고리: {todo.category}</span>
+              <span>우선순위: {todo.priority}</span>
+            </div>
+            <span className="text-sm text-gray-500">
+              {todo.completed ? "완료" : "진행중"}
+            </span>
+
+            <p className="mt-2 text-sm text-black-200">{todo.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
