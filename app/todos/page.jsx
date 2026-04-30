c982110540d7b37e1c77e7399b17d9daa6f1b888
Obebe <shkim000905@naver.com>
2026-04-30 14:41:44 +0900

    feat:todocard 공통 컴포넌트 분리

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -1,3 +1,5 @@
+import TodoCard from "@/components/TodoCard";
+
 export default async function TodosPage() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/todos`);
   const todos = await res.json();
@@ -8,18 +10,7 @@ export default async function TodosPage() {
 
       <ul className="grid grid-cols-2 gap-4">
         {todos.map((todo) => (
-          <li key={todo.id} className="rounded-md border border-black-100 p-4">
-            <h2 className="font-semibold">{todo.title}</h2>
-            <div className="mt-2 flex gap-2 text-xs text-gray-500">
-              <span>카테고리: {todo.category}</span>
-              <span>우선순위: {todo.priority}</span>
-            </div>
-            <span className="text-sm text-gray-500">
-              {todo.completed ? "완료" : "진행중"}
-            </span>
-
-            <p className="mt-2 text-sm text-black-200">{todo.content}</p>
-          </li>
+          <TodoCard key={todo.id} todo={todo} />
         ))}
       </ul>
     </div>
---
+++ components/TodoCard.jsx
@@ -0,0 +1,16 @@
+export default function TodoCard({ todo }) {
+  return (
+    <li key={todo.id} className="rounded-md border border-black-100 p-4">
+      <h2 className="font-semibold">{todo.title}</h2>
+      <div className="mt-2 flex gap-2 text-xs text-gray-500">
+        <span>카테고리: {todo.category}</span>
+        <span>우선순위: {todo.priority}</span>
+      </div>
+      <span className="text-sm text-gray-500">
+        {todo.completed ? "완료" : "진행중"}
+      </span>
+
+      <p className="mt-2 text-sm text-black-200">{todo.content}</p>
+    </li>
+  );
+}
